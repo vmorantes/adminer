f5e07506753b1fb9dfdef44cc31b8342007857d7
Vicsen Morantes <vicsen.mb@gmail.com>
2026-04-15 13:32:55 -0500

    Separación de vista del controlador en  AdminerSqlGemini

---
+++ adminer-plugins/AdminerSqlGemini/Form.php
@@ -0,0 +1,39 @@
+<?php
+    $textAreaPlaceHolder = $this->lang('Ask Gemini');
+?>
+
+<?= '<p>' ?><textarea name='gemini' rows='5' cols='50' placeholder='<?= $textAreaPlaceHolder ?>'><?= Adminer\h($_POST["gemini"]) ?></textarea>
+<?= '<p>' ?><input type='button' value='Gemini'>
+
+<script <?= Adminer\nonce(); ?>>
+const geminiText = qsl('textarea');
+const geminiButton = qsl('input');
+
+function setSqlareaValue(value) {
+    const sqlarea = qs('textarea.sqlarea');
+    sqlarea.value = value;
+    sqlarea.onchange && sqlarea.onchange();
+}
+geminiButton.onclick = () => {
+    setSqlareaValue('-- <?php echo $this->lang('Just a sec...'); ?>');
+    ajax(
+        '',
+        req => setSqlareaValue(req.responseText),
+        'gemini=' + encodeURIComponent(geminiText.value)
+    );
+};
+geminiText.onfocus = event => {
+    alterClass(findDefaultSubmit(geminiText), 'default');
+    alterClass(geminiButton, 'default', true);
+    event.stopImmediatePropagation();
+};
+geminiText.onblur = () => {
+    alterClass(geminiButton, 'default');
+};
+geminiText.onkeydown = event => {
+    if (isCtrl(event) && (event.keyCode == 13 || event.keyCode == 10)) {
+        geminiButton.onclick();
+        event.stopPropagation();
+    }
+};
+</script>
